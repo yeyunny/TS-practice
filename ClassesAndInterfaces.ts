1a938c7b692377c70781cbb32bc58ee67e4828f9
yeyun <hallo.yunye@gmail.com>
2023-07-11 13:48:16 +0900

    [ADD] override

--- ClassesAndInterfaces.ts
+++ ClassesAndInterfaces.ts
@@ -1,4 +1,5 @@
 // constructor 생성자
+// 추상 클래스인 User 안에
 abstract class User {
   constructor(
     protected firstName: string,
@@ -6,6 +7,7 @@ abstract class User {
     protected nickname: string
   ) {}
 
+  //void는 undefined로 찍힘
   abstract getNickName(): void;
 
   getFullName() {
@@ -18,6 +20,7 @@ class Player2 extends User {
     console.log(this.nickname);
   }
 
+  // override - 상속된 메소드와 다른 값을 리턴 할 수 있음
   getFullName() {
     return this.firstName;
   }
