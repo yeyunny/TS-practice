cd0726b1f20dd22456447f6fd4934e852e49c53c
yeyun <hallo.yunye@gmail.com>
2023-07-11 14:15:19 +0900

    [ADD] make Dictionary

---
+++ makeDict.ts
@@ -0,0 +1,35 @@
+type Words = {
+  [key: string]: string;
+};
+
+class Dict {
+  private words: Words;
+
+  constructor() {
+    this.words = {};
+  }
+
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+
+  def(term: string) {
+    return this.words[term];
+  }
+
+  del(term: string) {
+    return delete this.words[term];
+  }
+}
+
+class Word {
+  constructor(public term: string, public def: string) {}
+}
+
+const kimchi = new Word("kimchi", " 한국음식");
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.def("kimchi");
